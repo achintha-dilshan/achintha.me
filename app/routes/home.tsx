a889a429026c051be48878dac367dd0f6c949a89
ACHii <>
2025-02-21 19:50:01 +0530

    project cleanup

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,4 @@
-import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import type { Route } from "./+types/home"
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -13,5 +12,5 @@ export function loader({ context }: Route.LoaderArgs) {
 }
 
 export default function Home({ loaderData }: Route.ComponentProps) {
-  return <Welcome message={loaderData.message} />;
+  return <h1>Hello World!</h1>;
 }
